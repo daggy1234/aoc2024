dff651452626ffc3461937f36f9355e730a806d7
Arnav Jindal <arnav.jindal7@gmail.com>
2024-12-03 03:50:48 +0100

    complete day 1

--- haskell/Day01.hs
+++ haskell/Day01.hs
@@ -1,5 +1,5 @@
 module Day01 where
-import Data.List (sort)
+import Data.List (sort, group)
 import Data.List.Split (splitOn)
 
 parseLine :: String -> (Int, Int)
@@ -24,11 +24,24 @@ join _ [] = ""
 join delimiter (x:xs) = (show x) ++ delimiter ++ (join delimiter xs)
 
 freqCounter :: [Int] -> [(Int, Int)]
-freqCounter inp = map (\x -> ([head x], length x)) (group (sort inp))
+freqCounter inp = map (\x -> (head x, length x)) (group (sort inp))
 
 part1 :: String -> String
 part1 inp = show (processInps (parser inp))
 
+ctr :: [Int] -> [(Int, Int)] -> Int -> Int
+ctr [] _ summ = summ
+ctr (x:xs) freqs summ =
+    case filter (\(a, _) -> a == x) freqs of
+        [] -> ctr xs freqs summ
+        ((_, b):_) -> ctr xs freqs (summ + (b*x))
+
+part2 :: String -> String
+part2 inp = show (ctr a (freqCounter b) 0)  where (a,b) = parser inp
+
 solve :: String -> String
-solve inp = part1 inp
+solve inp =  mconcat ["Part 1: ", part1s, "\n", "Part 2: ", part2s]
+    where
+        part1s = part1 inp
+        part2s = part2 inp
 
